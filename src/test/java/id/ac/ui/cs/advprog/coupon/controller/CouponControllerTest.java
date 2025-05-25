caac70c4bd4ab1fca18f670064b0c1422b2e3b17
dzak27567 <fadhlurohmandzaki@gmail.com>
2025-05-25 14:42:02 +0700

    CHORE : remove unused import in couponcontrolletest

--- src/test/java/id/ac/ui/cs/advprog/coupon/controller/CouponControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/coupon/controller/CouponControllerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,8 +28,7 @@ public class CouponControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private CouponService couponService;
+
 
     @Autowired
     private ObjectMapper objectMapper;
